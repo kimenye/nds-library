897de19921f5714410ceeef48f651e8b959c1e5e
Trevor <trevor@Chaos-mac.local>
2013-11-20 13:19:53 +0300

    added initializer files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+NdsLibrary::Application.config.secret_key_base = '9e0dd8f9bda7f5210f6788ec07925b9f980c592ed5c891c6cad19ef5051b988991700c2431b10358ea3c99c28890be0a05ef8abc4dc044c83b681e168fa2d8c9'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+NdsLibrary::Application.config.session_store :cookie_store, key: '_nds-library_session'
